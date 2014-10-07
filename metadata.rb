894bb34c6fb889abd98c8af78d20bed421cf1e1b
Ruslan Khamidullin <rusllonrails@gmail.com>
2014-10-07 20:53:45 +0400

    Temprary fix for issue https://github.com/rackspace-cookbooks/varnish/issues/38

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Opscode, Inc.'
 maintainer_email 'cookbooks@opscode.com'
 license 'Apache 2.0'
 description 'Installs and configures varnish'
-version '1.0.0'
+version '1.0.1'
 
 recipe 'varnish', 'Installs and configures varnish'
 recipe 'varnish::repo', 'Adds the official varnish project repository'
@@ -14,6 +14,6 @@ end
 
 depends 'apt', '~> 2.4'
 depends 'build-essential'
-depends 'disable_ipv6'
+# depends 'disable_ipv6'
 depends 'yum', '~> 3.0'
 depends 'yum-epel'
